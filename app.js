d752cd160e92e31346f7edc5d007faaa4dd13e81
pratiksha17 <pratikshasingh175@gmail.com>
2019-02-21 16:58:16 +0530

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require ('express');
+const  mongoose= require('mongoose');
+let dev_db_url='mongodb://localhost:20701'
+let mongoDB=process.env.MONGODB_URI||dev_db_url_;
+mongoose.connect(mongoDB);
+mongoose.Promise=global.Promise;
+let db=mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDB connection error:'));
+var bodyParser = require('body-parser');
+const product = require('./routes/product.route');
+var app=express();
+var port=1234;
+app.use('/products',product);
+app.listen(port,()=>{
+console.log('Server is up '+port);});
